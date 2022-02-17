ebb906bc5dde9f6f75723a48a4b5c386a8583154
TARKZiM <tom8476oo@gmail.com>
2022-02-17 23:59:46 +0800

    msm8974-common: Re-enable GestureHandler

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -44,7 +44,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += FlipFlap
 
 # Gesture Handler
-# PRODUCT_PACKAGES += GestureHandler
+PRODUCT_PACKAGES += GestureHandler
 
 # Ramdisk
 PRODUCT_PACKAGES += \
